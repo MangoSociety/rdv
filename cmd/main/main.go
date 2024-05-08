bc010e9ff388ff7b09783eae29211ad64b22be38
MangoSociety <148572862+MangoSociety@users.noreply.github.com>
2024-05-09 03:22:22 +0400

    add fake response

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Hello, World!")
+		// Создание объекта пользователя
+		user := User{
+			Name:  "John Doe",
+			Email: "john@example.com",
+		}
+
+		// Установка заголовка Content-Type
+		w.Header().Set("Content-Type", "application/json")
+
+		// Кодирование объекта пользователя в JSON и запись ответа
+		err := json.NewEncoder(w).Encode(user)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	})
 
 	err := http.ListenAndServe(":5555", nil)
